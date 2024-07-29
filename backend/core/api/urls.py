b2dcfed63a57cbcf0e56739972b10d6c56353fcd
Kodeck James <kodeckjames095@gmail.com>
2024-07-29 12:09:46 +0300

    fourth commit

--- backend/core/api/urls.py
+++ backend/core/api/urls.py
@@ -1,2 +1,11 @@
-# define your urls:
-# define your urls:
+from rest_framework.routers import DefaultRouter
+from posts.api.urls import post_router
+from django.urls import path, include
+
+router = DefaultRouter()
+# Posts
+router.registry.extend(post_router.registry)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
