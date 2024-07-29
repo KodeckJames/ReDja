39541ff4f586846161fcdab79e0b70b45729cbdd
Kodeck James <kodeckjames095@gmail.com>
2024-07-29 17:58:37 +0300

    fifth commit

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 CORS_ALLOWED_ORIGINS  = ['http://localhost:5173']
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
+  const [data, setData] = useState([]);
+  
+  useEffect(() => {
+    async function fetchData() {
+      console.log(import.meta.env.VITE_API_URL);
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}posts`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        console.log(result);
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+    fetchData();
+  }, []);
+  
   
   return (
     <>
-     
+     <p>Hello World</p>
     </>
   )
 }
